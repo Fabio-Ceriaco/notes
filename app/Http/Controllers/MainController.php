1252b4885cf6cdaf0e0c1d5a0c9a65f1a5ce2b95
Fabio-Ceriaco <ceriacofabio@gmail.com>
2025-04-08 08:53:56 +0100

    Passing crypt ids and make private functions to decrypt

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -24,5 +27,35 @@ class MainController extends Controller
     {
 
         echo 'New note';
+    }
+
+    //================================================================
+    public function editNote($id)
+    {
+        $id = $this->decryptId($id);
+
+        echo $id;
+    }
+    //================================================================
+    public function deleteNote($id)
+    {
+        $id = $this->decryptId($id);
+
+        echo $id;
+    }
+
+    //================================================================
+    private function decryptId($id)
+    {
+
+        // check if id is encrypted
+        try {
+            // decrypt id
+            $id = Crypt::decrypt($id);
+        } catch (DecryptException $e) {
+            return redirect()->route('index');
+        }
+
+        return $id;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,15 @@ Route::middleware([CheckIsNotLogged::class])->group(function () {
 Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/', [MainController::class, 'index'])->name('index');
     Route::get('/newNote', [MainController::class, 'newNote'])->name('newNote');
+
+    // edit note
+    Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
+
+    // delete note
+    Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
+
+
+    // logout
+
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
